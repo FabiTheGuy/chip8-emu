b576387ccb06339d4fbf195eb76636060c20dabe
FabiTheGuy <fabian.doeller@mailfence.com>
2024-02-22 10:20:32 +0100

    edit/fix: cpu.cpp -> implemented Instructions + fixed faulty instructions + tested all instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -5,7 +5,11 @@
 
 #include "../include/flash-memory.hpp"
 #include "../include/utils.hpp"
-#define USED_AND_BREAK { inst_used = true; break; }
+#define USED_AND_BREAK \
+  {                    \
+    inst_used = true;  \
+    break;             \
+  }
 
 void CPU::reset(void) {
   /* Clears the memory */
@@ -47,175 +51,190 @@ void CPU::load_program(byte* program, word program_size) {
 
 void CPU::run_cycle(void) {
   word inst = fetch() << 8 | fetch(); /* Fetches 2 Bytes merged together */
-  byte x_index = (inst & 0x0F00) >> 8; /* Determines Bit 8-11 (in some cases x-Address) */
-  byte y_index = (inst & 0x00F0) >> 4; /* Determines Bit 4-7 (in some cases y-Address) */
+  byte x_index =
+      (inst & 0x0F00) >> 8; /* Determines Bit 8-11 (in some cases x-Address) */
+  byte y_index =
+      (inst & 0x00F0) >> 4; /* Determines Bit 4-7 (in some cases y-Address) */
   bool inst_used = false;
-
 
   /* Checks Instructions by all 16-Bits (0x(0-F)(0-F)(0-F)(0-F)) */
   switch (inst) {
-      case INS_CLS : {
-        for (byte index = 0; index < 0x20; index++) {
-            memset(scr_buff[index], 0, 0x40);
-        }
-        USED_AND_BREAK
+    case INS_CLS: {
+      for (byte index = 0; index < 0x20; index++) {
+        memset(scr_buff[index], 0, 0x40);
+      }
+      USED_AND_BREAK
     }
-    case INS_RET : {
-        program_counter = stack[stack_pointer--];
-        USED_AND_BREAK
+    case INS_RET: {
+      program_counter = stack[stack_pointer--];
+      USED_AND_BREAK
     }
   }
 
   /* Checks Instructions by first 4-Bits (0x(0-F)XXX) */
   switch (inst & 0xF000) {
-      case INS_JP_ADDR : {
-          program_counter = inst & 0x0FFF;
-          USED_AND_BREAK
-      }
-      case INS_CALL_ADDR : {
-          stack[stack_pointer++] = program_counter;
-          program_counter = inst & 0x0FFF;
-          USED_AND_BREAK
-      }
-      case INS_SE_VX_BYTE : {
-          program_counter = (reg_v[x_index] == (inst & 0x00FF)) ? program_counter + 2 : program_counter;
-          USED_AND_BREAK
-      }
-      case INS_SNE_VX_BYTE : {
-          program_counter = (reg_v[x_index] != (inst & 0x00FF)) ? program_counter + 2 : program_counter;
-          USED_AND_BREAK
-      }
-      case INS_SE_VX_VY : {
-          program_counter = (reg_v[x_index] == reg_v[y_index]) ? program_counter + 2 : program_counter;
-          USED_AND_BREAK
-      }
-      case INS_LD_VX_BYTE : {
-          reg_v[x_index] = inst & 0x00FF;
-          USED_AND_BREAK
-      }
-      case INS_ADD_VX_BYTE : {
-          reg_v[x_index] += inst & 0x00FF;
-          USED_AND_BREAK
-      }
-      case INS_SNE_VX_VY : {
-          program_counter = (reg_v[x_index] != reg_v[y_index]) ? program_counter + 2 : program_counter;
-          USED_AND_BREAK
-      }
-      case INS_LD_I_ADDR : {
-          reg_index = inst & 0x0FFF;
-          USED_AND_BREAK
-      }
-      case INS_JP_V0_ADDR : {
-          program_counter = (inst & 0x0FFF) + reg_v[0x00];
-          USED_AND_BREAK
-      }
-      case INS_RND_VX_BYTE : {
-          reg_v[x_index] = random_range(0, 255) & (inst & 0x00FF);
-          USED_AND_BREAK
-      }
-      case INS_DRW_VX_VY_NIBBLE : {
-          /* TODO: after implementation of screen */
-          USED_AND_BREAK
-      }
+    case INS_JP_ADDR: {
+      program_counter = inst & 0x0FFF;
+      USED_AND_BREAK
+    }
+    case INS_CALL_ADDR: {
+      stack[stack_pointer++] = program_counter;
+      program_counter = inst & 0x0FFF;
+      USED_AND_BREAK
+    }
+    case INS_SE_VX_BYTE: {
+      program_counter = (reg_v[x_index] == (inst & 0x00FF))
+                            ? program_counter + 2
+                            : program_counter;
+      USED_AND_BREAK
+    }
+    case INS_SNE_VX_BYTE: {
+      program_counter = (reg_v[x_index] != (inst & 0x00FF))
+                            ? program_counter + 2
+                            : program_counter;
+      USED_AND_BREAK
+    }
+    case INS_SE_VX_VY: {
+      program_counter = (reg_v[x_index] == reg_v[y_index]) ? program_counter + 2
+                                                           : program_counter;
+      USED_AND_BREAK
+    }
+    case INS_LD_VX_BYTE: {
+      reg_v[x_index] = inst & 0x00FF;
+      USED_AND_BREAK
+    }
+    case INS_ADD_VX_BYTE: {
+      reg_v[x_index] += inst & 0x00FF;
+      USED_AND_BREAK
+    }
+    case INS_SNE_VX_VY: {
+      program_counter = (reg_v[x_index] != reg_v[y_index]) ? program_counter + 2
+                                                           : program_counter;
+      USED_AND_BREAK
+    }
+    case INS_LD_I_ADDR: {
+      reg_index = inst & 0x0FFF;
+      USED_AND_BREAK
+    }
+    case INS_JP_V0_ADDR: {
+      program_counter = (inst & 0x0FFF) + reg_v[0x00];
+      USED_AND_BREAK
+    }
+    case INS_RND_VX_BYTE: {
+      reg_v[x_index] = random_range(0, 255) & (inst & 0x00FF);
+      USED_AND_BREAK
+    }
+    case INS_DRW_VX_VY_NIBBLE: {
+      /* TODO: after implementation of screen */
+      USED_AND_BREAK
+    }
   }
 
   /* Checks Instructions by first/last 4-Bits (0x(0-F)XX(0-F)) */
   switch (inst & 0xF00F) {
-      case INS_LD_VX_VY : {
-          reg_v[x_index] = reg_v[y_index];
-          USED_AND_BREAK
-      }
-      case INS_OR_VX_VY : {
-          reg_v[x_index] |= reg_v[y_index];
-          USED_AND_BREAK
-      }
-      case INS_AND_VX_VY : {
-          reg_v[x_index] &= reg_v[y_index];
-          USED_AND_BREAK
-      }
-      case INS_XOR_VX_VY : {
-          reg_v[x_index] ^= reg_v[y_index];
-          USED_AND_BREAK
-      }
-      case INS_ADD_VX_VY : {
-          word result = reg_v[x_index] + reg_v[y_index];
-          reg_v[0x0F] = result > 0xFF;
-          reg_v[x_index] = result & 0x00FF;
-          USED_AND_BREAK
-      }
-      case INS_SUB_VX_VY : {
-          reg_v[0x0F] = reg_v[x_index] > reg_v[y_index];
-          reg_v[x_index] -= reg_v[y_index];
-          USED_AND_BREAK
-      }
-      case INS_SHR_VX_VY : {
-          reg_v[0x0F] = reg_v[x_index] & 0x01;
-          reg_v[x_index] /= 2;
-          USED_AND_BREAK
-      }
-      case INS_SUBN_VX_VY : {
-          reg_v[0x0F] = reg_v[y_index] > reg_v[x_index];
-          reg_v[x_index] = reg_v[y_index] - reg_v[x_index];
-          USED_AND_BREAK
-      }
+    case INS_LD_VX_VY: {
+      reg_v[x_index] = reg_v[y_index];
+      USED_AND_BREAK
+    }
+    case INS_OR_VX_VY: {
+      reg_v[x_index] |= reg_v[y_index];
+      USED_AND_BREAK
+    }
+    case INS_AND_VX_VY: {
+      reg_v[x_index] &= reg_v[y_index];
+      USED_AND_BREAK
+    }
+    case INS_XOR_VX_VY: {
+      reg_v[x_index] ^= reg_v[y_index];
+      USED_AND_BREAK
+    }
+    case INS_ADD_VX_VY: {
+      word result = reg_v[x_index] + reg_v[y_index];
+      reg_v[0x0F] = result > 0xFF;
+      reg_v[x_index] = result & 0x00FF;
+      USED_AND_BREAK
+    }
+    case INS_SUB_VX_VY: {
+      reg_v[0x0F] = reg_v[x_index] > reg_v[y_index];
+      reg_v[x_index] -= reg_v[y_index];
+      USED_AND_BREAK
+    }
+    case INS_SHR_VX_VY: {
+      reg_v[0x0F] = reg_v[x_index] & 0x01;
+      reg_v[x_index] /= 2;
+      USED_AND_BREAK
+    }
+    case INS_SUBN_VX_VY: {
+      reg_v[0x0F] = reg_v[y_index] > reg_v[x_index];
+      reg_v[x_index] = reg_v[y_index] - reg_v[x_index];
+      USED_AND_BREAK
+    }
+    case INS_SHL_VX_VY: {
+      reg_v[0x0F] = reg_v[x_index] >> 7;
+      reg_v[x_index] *= 2;
+      USED_AND_BREAK
+    }
   }
 
   /* Checks Instructions by all Bits except of Bits 8-11 */
   switch (inst & 0xF0FF) {
-      case INS_SKP_VX : {
-          /* TODO: after implementation of keyboard */
-          USED_AND_BREAK
-      }
-      case INS_SKNP_VX : {
-          /* TODO: after implementation of keyboard */
-          USED_AND_BREAK
-      }
-      case INS_LD_VX_DT : {
-          reg_v[x_index] = delay_timer;
-          USED_AND_BREAK
-      }
-      case INS_LD_VX_K : {
-          /* TODO: after implementation of keyboard */
-          USED_AND_BREAK
-      }
-      case INS_LD_DT_VX : {
-          delay_timer = reg_v[x_index];
-          USED_AND_BREAK
-      }
-      case INS_LD_ST_VX : {
-          sound_timer = reg_v[x_index];
-          USED_AND_BREAK
-      }
-      case INS_LD_F_VX : {
-          reg_index = memory[0x50 + reg_v[x_index]];
-          USED_AND_BREAK
-      }
-      case INS_LD_B_VX : {
-          byte hundrets = (reg_v[x_index] / 100) * 100;
-          byte tenths = (reg_v[x_index] - hundrets / 10) * 10;
-          byte ones = reg_v[x_index] - tenths;
+    case INS_SKP_VX: {
+      /* TODO: after implementation of keyboard */
+      USED_AND_BREAK
+    }
+    case INS_SKNP_VX: {
+      /* TODO: after implementation of keyboard */
+      USED_AND_BREAK
+    }
+    case INS_LD_VX_DT: {
+      reg_v[x_index] = delay_timer;
+      USED_AND_BREAK
+    }
+    case INS_LD_VX_K: {
+      /* TODO: after implementation of keyboard */
+      USED_AND_BREAK
+    }
+    case INS_LD_DT_VX: {
+      delay_timer = reg_v[x_index];
+      USED_AND_BREAK
+    }
+    case INS_LD_ST_VX: {
+      sound_timer = reg_v[x_index];
+      USED_AND_BREAK
+    }
+    case INS_ADD_I_VX: {
+      reg_index += reg_v[x_index];
+      USED_AND_BREAK
+    }
+    case INS_LD_F_VX: {
+      reg_index = 0x50 + (reg_v[x_index] * 5);
+      USED_AND_BREAK
+    }
+    case INS_LD_B_VX: {
+      byte hundrets = (reg_v[x_index] / 100) * 100;
+      byte tenths = ((reg_v[x_index] - hundrets) / 10) * 10;
+      byte ones = reg_v[x_index] - hundrets - tenths;
 
-          memory[reg_index] = hundrets;
-          memory[reg_index + 1] = tenths;
-          memory[reg_index + 2] = ones;
-          USED_AND_BREAK
+      memory[reg_index] = hundrets;
+      memory[reg_index + 1] = tenths;
+      memory[reg_index + 2] = ones;
+      USED_AND_BREAK
+    }
+    case INS_LD_I_VX: {
+      for (byte index = 0; index <= ((inst & 0x0F00) >> 8); index++) {
+        memory[reg_index + index] = reg_v[index];
       }
-      case INS_LD_I_VX : {
-          for (byte index = 0; index < 0x0F; index++) {
-              memory[reg_index + index] = reg_v[index];
-          }
-          USED_AND_BREAK
+      USED_AND_BREAK
+    }
+    case INS_LD_VX_I: {
+      for (byte index = 0; index < ((inst & 0x0F00) >> 8); index++) {
+        reg_v[index] = memory[reg_index + index];
       }
-      case INS_LD_VX_I : {
-          for (byte index = 0; index < 0x0F; index++) {
-              reg_v[index] = memory[reg_index + index];
-          }
-          USED_AND_BREAK
-      }
+      USED_AND_BREAK
+    }
 
       if (!inst_used) {
-          std::fprintf(stderr, "Instruction: %04x is unknown!\n", inst);
+        std::fprintf(stderr, "Instruction: %04x is unknown!\n", inst);
       }
   }
-
 }
